704eddf35564ec3192fb435bee740192c75d2dec
Torkel Rogstad <torkel@rogstad.io>
2019-02-12 15:59:37 +0100

    Add clightning support

--- src/lightning/clightning.ts
+++ src/lightning/clightning.ts
@@ -1,1 +1,48 @@
-// TODO
+import assert from 'assert'
+import makeDebug from 'debug'
+import fsCb from 'fs'
+import LightningClient from 'lightning-client'
+import os from 'os'
+import path from 'path'
+import util from 'util'
+import uuid from 'uuid'
+
+import { LightningApi } from '.'
+
+const debug = makeDebug('lightning:clightning')
+const lstat = util.promisify(fsCb.lstat)
+
+export interface CLightning extends LightningApi {}
+
+class CLightningImpl implements LightningApi {
+  private client: LightningClient
+  public constructor(rpcPath: string) {
+    debug(`Connecting to c-lightning with rpcPath ${rpcPath}`)
+    this.client = new LightningClient(rpcPath)
+  }
+
+  public receive = (description: string = ''): Promise<string> => {
+    const uniqueLabel = uuid() // c-lightning requires all invoice labels to be unique
+    const amountMsat = 'any'
+    return this.client.invoice(amountMsat, uniqueLabel, description).then(({ bolt11 }) => bolt11)
+  }
+
+  public send = (invoice: string): Promise<any> => this.client.pay(invoice)
+
+  public getInfo = (): Promise<any> => this.client.getinfo()
+}
+
+const DEFAULT_DATADIR = path.join(os.homedir(), '.lightning')
+export const CLightning = async (rpcPath: string = DEFAULT_DATADIR): Promise<CLightning> => {
+  debug(`rpcPath: ${rpcPath}`)
+  const lstats = await lstat(rpcPath)
+  const isDirectory = lstats.isDirectory
+  assert(isDirectory, `${rpcPath} is not a directory!`)
+
+  const client = new CLightningImpl(rpcPath)
+  debug('Trying to connect to client')
+  const info = await client.getInfo()
+  debug('Succeeded!')
+  debug('getinfo output: %o', info)
+  return client
+}
