a4b464573193d4ff29f2a1bd416fcd8567556bbb
Torkel Rogstad <torkel@rogstad.io>
2019-07-22 18:18:16 +0200

    v0.3.1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { BitcoinNetwork } from './lightning';
+export { BitcoinNetwork } from './lightning'
 export { Sockets } from './sockets'
 export { HistoricalRestAPI } from './rest'
 export { CLightning, Eclair, Lnd } from './lightning'
